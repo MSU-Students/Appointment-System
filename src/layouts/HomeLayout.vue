436a9c19a36ceff3673d1836f6246cd798d7b053
BillGates001 <ampuann98@gmail.com>
2021-08-30 12:35:03 -1200

    Changes for landing page and creating Home Page

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,110 @@
+<template>
+ 
+  <q-layout view="hHh lpR fFf">
+      <q-page class="bg-image">
+ 
+    <q-header elevated>
+        <q-toolbar class="bg-black">
+            <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+                <q-toolbar-title>MSU APPOINTMENT SYSTEM</q-toolbar-title>
+
+                <div class="q-pa-xss q-gutter-sm flex flex-center">
+                    <q-btn outline rounded color="white" label="Log Out" class="bg-transparent" to="signed"></q-btn>
+                     <q-btn flat round dense icon="more_vert" />  
+                </div>
+        </q-toolbar>
+    </q-header>
+
+<q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="250"
+        :breakpoint="500"
+        bordered
+        class="bg-white"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+      <q-page-container>
+        <q-page>
+        </q-page>
+      </q-page-container>
+
+</q-page>
+
+  </q-layout>
+ 
+</template>
+<script>
+import { ref } from 'vue'
+
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Dashboard',
+    separator: true
+  },
+  {
+    icon: 'send',
+    label: 'New Appointment',
+    separator: false
+  },
+  {
+    icon: 'help',
+    label: 'Search Appointment',
+    separator: false
+  },
+  {
+    icon: 'error',
+    label: 'Schedule',
+    separator: true
+  },
+  {
+    icon: 'settings',
+    label: 'Administrator',
+    separator: false
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false
+  }
+  
+]
+
+export default {
+  setup () {
+    return {
+      drawer: ref(false),
+      menuList
+    }
+  }
+}
+</script>
+<style>
+.bg-image
+{
+  background-image: url(../assets/image/imagess.jpeg);
+  background-blend-mode: overlay;
+  background-position: center;
+  background-size: 100%;
+}
+
+</style>
+
--- src/pages/signed.vue
+++ src/pages/signed.vue
@@ -1,51 +1,74 @@
 <template>
   <q-page class="bg-image">
-    <div class="absolute-center">
+    <div class="q-pl-lg absolute-center">
+      <div class="q-pr-xl">
+        <div class="q-pr-xl">
+          <div class="q-pr-xl">
+            <div class="q-pr-xl">
+              <div class="q-pr-xl">
+                <div class="q-pr-xl">
+                  <div class="q-pr-xl">
+                    <div class="q-pr-xl">
+                      
       
-      <q-card class="bg-black text-center">
-      
-        <q-card-section style="width: 360px"></q-card-section>
+                          <div class="text-h1 text-bold text-arial text-black flex flex-left"> MSU APPOINTMENT SYSTEM </div>
+                        
+                          
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="absolute-right q-pr-xl">
+      <div class="q-pr-xl">
+        <div class="q-pr-xl">
+      <div class="q-pt-xl"></div>
+      <div class="q-pt-xl"></div>
+      <div class="q-pt-xl"></div>
+      <div class="q-pt-md"></div>
+
+       <q-card class="bg-white text-center q-pb-md">
+        <q-card-section style="width: 350px"></q-card-section>
+
+       <div class="text-h5 text-bold text-arial text-black flex flex-center q-pb-md"> Log in to continue </div>
+            <div class="q-pa-xs flex flex-center">
+               <q-input
+                filled
+                v-model="text"
+                label="username"
+                style="width: 300px" color="black" bg-color="grey-3" label-color="black"/>
+            </div>
        
-        <div class="q-pa-xs flex flex-center">
-          <q-input
-            filled
-            v-model="text"
-            label="username"
-            style="width: 350px" color="black" bg-color="grey-3" label-color="black" 
-          />
-        </div>
-       
-        <div class="q-pa-xs flex flex-center">
-          <q-input
-            filled
-            v-model="password"
-            label="password"
-            type="password"
-            style="width: 350px" color="black" bg-color="white" label-color="black" 
-          >
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-        </div>
-        <div class="q-pa-lg q-gutter-sm flex flex-center">
-          
-           <q-btn
-      v-for="size in sizes" :key="`btn_size_rd_${size}`"
+            <div class="q-pa-xs flex flex-center">
+              <q-input
+                filled
+                v-model="password"
+                label="password"
+                type="password"
+                style="width: 300px" color="black" bg-color="grey-4" label-color="black">
+                  <template v-slot:append>
+                      <q-icon
+                        :name="isPwd ? 'visibility_off' : 'visibility'"
+                        class="cursor-pointer"
+                        @click="isPwd = !isPwd"/>
+                  </template>
+              </q-input>
+            </div>
+
+            <div class="q-pa-md q-gutter-sm flex flex-center text-bold">
+             
+                <q-btn label="Log In" style="width:150px" class="text-bold bg-black text-white flex flex-center" text-outlined="white" to="Homelayout"/>
+            </div>
+    
       
-      color="white"
-      :size="size"
-      :label="`Log In`"
-      :text-color="'black'"
-    />
-        </div>
       </q-card>
-     
+    </div>
+    </div>
     </div>
   </q-page>
 </template>
@@ -59,7 +82,7 @@ export default {
       text: ref(''),
        password: ref(''),
       isPwd: ref(true),
-      sizes: ['md'],
+    
     };
   },
 };
